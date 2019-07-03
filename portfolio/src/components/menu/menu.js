40a1e4094915ce14df5cb97adbe010e0045c953a
Przemyslaw Leksa <38156110+gnoosny@users.noreply.github.com>
2019-07-03 20:17:26 +0200

    Begining

---
+++ portfolio/src/components/menu/menu.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './menu.scss';
+
+
+const Menu = () => {
+    return ( 
+        <nav>
+            <div className="burger"></div>
+            <ul>
+                <a href="#">
+                <li>
+                    Home
+                </li>
+                </a>
+                <a href="#">
+                <li>
+                    About
+                </li>
+                </a>
+                <a href="#">
+                <li>
+                    Stack
+                </li>
+                </a>
+                <a href="#">
+                <li>
+                    Portfolio
+                </li>
+                </a>
+                <a href="#">
+                <li>
+                    Contact
+                </li>
+                </a>
+            </ul>
+        </nav>
+     );
+}
+ 
+export default Menu;
+<nav>
+
+</nav>
