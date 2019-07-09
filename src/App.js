136b0913c16ecc1bee7de31ca489d7f54df38a4e
Przemyslaw Leksa <38156110+gnoosny@users.noreply.github.com>
2019-07-09 21:32:11 +0200

    123

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Homepage from './pages/homepage/homepage';
+
+
+function App() {
+  return (
+    <div className="App" id="app">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
